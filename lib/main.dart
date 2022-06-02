42722e88588d1ee225c6ed969ed8980ce80396ca
xaldarof <temuruz2003@gmail.com>
2022-06-02 15:02:42 +0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled/secondScreen.dart';
@@ -20,7 +22,6 @@ class User {
 
 class _AppState extends State<App> {
   List<User> items = [];
-
 
   @override
   void initState() {
@@ -69,16 +70,24 @@ class _AppState extends State<App> {
                                     borderRadius:
                                         BorderRadius.all(Radius.circular(24))),
                                 child: Container(
-                                  alignment: Alignment.centerLeft,
-                                  padding: const EdgeInsets.all(12),
-                                  child: Text(
-                                    items[index].name,
-                                    style: const TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18),
-                                  ),
-                                ),
+                                    alignment: Alignment.centerLeft,
+                                    padding: const EdgeInsets.all(12),
+                                    child: Row(children: [
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.circular(18),
+                                        child: Image.network(
+                                            'https://picsum.photos/250?image=9'),
+                                      ),
+                                      const Padding(
+                                          padding: EdgeInsets.all(12)),
+                                      Text(
+                                        items[index].name,
+                                        style: const TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18),
+                                      )
+                                    ])),
                               ))),
                     ])),
                 onDismissed: (direction) {
---
+++ lib/navigation.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'HOME PAGE',
+      style: optionStyle,
+    ),
+    Text(
+      'COURSE PAGE',
+      style: optionStyle,
+    ),
+    Text(
+      'CONTACT GFG',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GeeksForGeeks'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Center(
+          child: ClipRRect(
+            borderRadius:
+            BorderRadius.circular(24),
+            child: Image.network('https://picsum.photos/250?image=9'),
+          ),
+        )
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: ('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark),
+            label: 'Courses',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_mail),
+            label: 'Mail',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
